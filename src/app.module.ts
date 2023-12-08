0fceb3e04dee8f7e23ddf560d2ea795d8d4aaae3
GRPropik <78607300+Gustavitory@users.noreply.github.com>
2023-12-08 01:13:50 -0400

    Coneccion a la db mongo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseModule } from './modules/database/database.module';
+import { ConfigModule } from '@nestjs/config';
+import config from './env/config';
+import validations from './env/validations';
+import { enviroments } from './env/enviroment';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: enviroments[process.env.NODE_ENV] || '.env',
+      load: [config],
+      validationSchema: validations,
+    }),
+    DatabaseModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
